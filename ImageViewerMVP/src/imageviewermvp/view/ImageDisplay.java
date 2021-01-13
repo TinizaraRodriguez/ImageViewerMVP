b31a522c0eebde6bdccd1ffb393d352fdce0e186
Tinizara Rodriguez <72140889+TinizaraRodriguez@users.noreply.github.com>
2021-01-13 09:43:20 +0000

    Add files via upload

---
+++ ImageViewerMVP/src/imageviewermvp/view/ImageDisplay.java
@@ -0,0 +1,15 @@
+package imageviewermvp.view;
+
+import imageviewermvp.model.Image;
+
+public interface ImageDisplay {
+    
+    void display(Image image);
+    Image image();
+    void on(Shift shift);
+    
+    interface Shift {
+        Image left();
+        Image rigth();
+    }
+}
